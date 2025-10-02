ef1e16f6cfc8a65faa06943590ae79e423904370
Michał Płudowski <michal.pludowski@codema.pl>
2025-10-02 09:59:02 +0200

    changes :V

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
 alias please="sudo"
 alias dco="docker compose"
+alias pco="podman-compose"
 alias dff="kitty +kitten diff"
+alias lst="last -s today -F"
 
 set -x MINIKUBE_IN_STYLE true
 set -g theme_newline_cursor yes
@@ -13,10 +15,11 @@ function dr
     docker run --rm -it -v (pwd):/app -w /app $argv
 end
 
+
 function dps
     for i in (docker ps --no-trunc --format '{{json .}}')
         set i (echo $i | jq "{Names: .Names,ID: .ID,Image: .Image,Ports: .Ports,Command: .Command,CreatedAt: .CreatedAt,LocalVolumes: .LocalVolumes,Mounts: .Mounts,Networks: .Networks,RunningFor: .RunningFor,Size: .Size,Status: .Status}")
-        echo $i | json2yaml
+        echo $i | yq -y #| json2yaml
         echo ''
     end | ccat
 end
